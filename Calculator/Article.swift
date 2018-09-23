c20acdd38fa4f15a5bcdb20e3cdc03ffc25f79b2
HGPMac63 <hgpmac63@HGPAdmins-Air-7.attlocal.net>
2018-09-23 15:11:37 -0700

    first commit

---
+++ Calculator/Article.swift
@@ -0,0 +1,13 @@
+//
+//  Article.swift
+//  Calculator
+//
+//  Created by HGPMac63 on 9/22/18.
+//  Copyright © 2018 Bossgodacid. All rights reserved.
+//
+
+import UIKit
+
+class Article: NSObject {
+
+}
---
+++ Calculator/ArticleCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleCell.swift
+//  Calculator
+//
+//  Created by HGPMac63 on 9/22/18.
+//  Copyright © 2018 Bossgodacid. All rights reserved.
+//
+
+import UIKit
+
+class ArticleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
